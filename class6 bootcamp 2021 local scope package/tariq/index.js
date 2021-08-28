4d69dc755b713015cb7e929fae417df413ff0f99
Tariq15994 <tariqnawaz15994@gmail.com>
2021-08-28 11:58:27 +0500

    class 6 work
    In tariq folder I create my first Hello world js package and deploy on node  server

---
+++ class6 bootcamp 2021 local scope package/tariq/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.details = void 0;
+function details() {
+    return console.log("Hello World, From bootcamp-urdu-sesseion team");
+}
+exports.details = details;
